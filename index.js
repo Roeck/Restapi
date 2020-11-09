adb16885300afdb1d33cf6eb77707e1b47190748
Roeck <rons9n@gmail.com>
2020-11-09 17:41:28 -0500

    added first route

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import usersRoutes from './routes/users.js';
 
 const app=express();
 const PORT=5000;
 
 app.use(bodyParser.json());
 
-app.listen(PORT,() => console.log(`Server running`));
+app.get('/',(req,res) => res.send("Hello"));
+
+app.listen(PORT,() => console.log(`Server running on port: http://localhost:${PORT}`));
